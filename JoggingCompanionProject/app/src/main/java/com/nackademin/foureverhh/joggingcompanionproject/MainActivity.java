0a534fef35dff1e905398c93ccc985bfeef4ada1
foureverhh <foureverhh@gmail.com>
2018-03-06 18:35:41 +0100

    Empty activity

---
+++ JoggingCompanionProject/app/src/main/java/com/nackademin/foureverhh/joggingcompanionproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nackademin.foureverhh.joggingcompanionproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //A good begin to github
+    }
+}
